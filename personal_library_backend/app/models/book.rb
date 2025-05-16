7a917d839bdb0b4edc5ad70ec30035ecc513f1a2
martinmt-mx <martin_4-3@hotmail.com>
2025-05-15 21:56:37 -0600

    Reconfiguration on Book Model

--- personal_library_backend/app/models/book.rb
+++ personal_library_backend/app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ApplicationRecord
-    enum status: { to_read: 0, reading: 1, finished: 2 }
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :status, inclusion: { in: statuses.keys }
-    validates :rating, numericality: { only_integer: true, allow_nil: true }, inclusion: { in: 1..5, allow_nil: true }
+  # Definimos el enum para el estado
+  enum status: { to_read: 0, reading: 1, finished: 2 }
+  
+  # Validaciones
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :status, inclusion: { in: statuses.keys }
+  validates :rating, numericality: { only_integer: true, allow_nil: true }, inclusion: { in: 1..5, allow_nil: true }
 end
